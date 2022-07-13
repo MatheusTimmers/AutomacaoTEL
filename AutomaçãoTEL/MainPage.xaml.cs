da01a815ff941ab5d64797687cfc5b4ec9cd164a
MatheusTimmers <matheus.timmers@edu.pucrs.br>
2022-07-13 03:54:44 -0300

    v0.1.2
    Criando as paginas de cada Automação

--- AutomaçãoTEL/MainPage.xaml.cs
+++ AutomaçãoTEL/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,7 +13,9 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using muxc = Microsoft.UI.Xaml.Controls;
 
 // O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
 
@@ -23,12 +26,52 @@ namespace AutomaçãoTEL
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        muxc.NavigationViewItem _lastItem;
+
         public MainPage()
         {
             this.InitializeComponent();
 
             ApplicationView.PreferredLaunchViewSize = new Size(420, 650);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(muxc.NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is muxc.NavigationViewItem && item.Tag.ToString() == "Home")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+            ContentFrame.Navigate(typeof(AutomaçãoTEL.Views.Home));
+        }
+
+
+        private bool NavigateToView(string clickedView)
+        {
+            var view = Assembly.GetExecutingAssembly().GetType($"AutomaçãoTEL.Views.{clickedView}");
+
+
+
+            if (string.IsNullOrWhiteSpace(clickedView) || view == null)
+                return false;
+
+            ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+            return true;
+        }
+
+        private void NavView_ItemInvoked(muxc.NavigationView sender, muxc.NavigationViewItemInvokedEventArgs args)
+        {
+            var item = args.InvokedItemContainer as muxc.NavigationViewItem;
+            if (item == null || item == _lastItem)
+                return;
+            var clickedView = item.Tag?.ToString() ?? "Config";
+            if (!NavigateToView(clickedView)) return;
+            _lastItem = item;
         }
     }
 }
